dc625e798d74a4fc132d5a112221f3d5a163f0f5
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-14 23:26:20 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName='bob';
+
+function upperCaseCustomerName(el) {
+    customerName;
+    customerName = customerName.toUpperCase();
+}
+
+//upperCaseCustomerName(customerName)
+
+
+
+function setBestCustomer() {
+
+    bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+
+    bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer="james"
+
+function changeLeastFavoriteCustomer() {
+
+    leastFavoriteCustomer="keke"
+
+}
